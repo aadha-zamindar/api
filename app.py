ee7abc952a254f1afaf1c10c06cc59693e123b4b
aviral <aviral@terpmail.umd.edu>
2020-02-01 01:16:13 -0500

    mongo DB Atlas is live

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 from flask import Flask, jsonify, request
-from flask.ext.pymongo import pymongo
 import os
+import pymongo
+from pprint import pprint
+import datetime
 
-
+# Intialize
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+
+# app.congig['MONGO_DBNAME'] = 'zamindar'
+
+# Database 
+mongo = pymongo.MongoClient('mongodb+srv://avi123:avi123@cluster0-xfmlr.gcp.mongodb.net/test?retryWrites=true&w=majority',
+ maxPoolSize=50, connect=False)
+db = mongo.testingseller
+collection = db.seller
+
+user = {
+  "name": { "first": "Alan", "last": "Turing" },
+  "birth": datetime.datetime(1912, 6, 23),
+  "death": datetime.datetime(1954, 6, 7),
+  "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+  "views": 1250000
+}
+collection.insert_one(user)
+
+
+
+
+@app.route('/',methods=['GET'])
+def get():
+    return jsonify({'addha-zamindar' : 'Soumya Jagdev'})
 
 ## Run Server 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
